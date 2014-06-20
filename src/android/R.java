0f2ec3ed154478ed9f2a744bb67c6e2d8c3cbc01
INAPP <imapp.help@gmail.com>
2014-06-20 16:22:06 -0700

    .

--- src/android/R.java
+++ src/android/R.java
@@ -1,11 +1,11 @@
 package kr.imapp.hybrid.imhotissue.settings;
 
-public class R {
-	public class string {
+public static class R {
+	public static class string {
 		public static String color_pick;
 	};
 	
-	public class xml {
+	public static class xml {
 		public static int setup;
 	}
 }
